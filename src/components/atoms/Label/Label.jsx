ecbacfb425a26dd6cb308ef1d6f019fcbd9c4735
marboqmar <martamartinezboquer@gmail.com>
2026-01-13 12:05:47 +0100

    Added label component

---
+++ src/components/atoms/Label/Label.jsx
@@ -0,0 +1,27 @@
+import './Label.css';
+import { classNames } from '../../../utils/helpers';
+
+/**
+ * Label component
+ *
+ * Component props:
+ * @param {string} [className] - Additional CSS classes.
+ * @param {string} htmlFor - ID of the associated input element.
+ * @param {object} [rest] - Additional props passed.
+ * @param {node} children - Label content.
+ * @param {boolean} required - Adds '*' to indicate this field is required.
+ *
+ * @example
+ * <Label htmlFor="email-field" required>
+ *   Email Address
+ * </Label>
+ */
+
+export const Label = ({ className = '', htmlFor, children, required, ...rest }) => {
+  return (
+    <label htmlFor={htmlFor} className={classNames('input-label', className)} {...rest}>
+      {children}
+      {required && <span className="input-label__required"> *</span>}
+    </label>
+  );
+};
