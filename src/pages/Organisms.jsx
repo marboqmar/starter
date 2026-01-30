d3edd9834cf56468779104f880a75ad1fa838ee7
marboqmar <martamartinezboquer@gmail.com>
2026-01-30 11:39:44 +0100

    Added basic organisms page structure

--- src/pages/Organisms.jsx
+++ src/pages/Organisms.jsx
@@ -1,5 +1,20 @@
+import '../css/pages/showcase.css';
+import { Breadcrumbs } from '../components/molecules/Breadcrumbs/Breadcrumbs';
 import { Heading } from '../components/atoms/Heading/Heading';
 
+const breadcrumbsInfo = [
+  { label: 'Home', href: '/' },
+  { label: 'Organisms', href: '/organisms' },
+];
+
 export const Organisms = () => {
-  return <Heading level={1}>Organisms</Heading>;
+  return (
+    <div className="organisms-page container">
+      <Breadcrumbs items={breadcrumbsInfo} />
+      <Heading level={1} className="showcase-page__title">
+        Organisms
+      </Heading>
+      <div className="organisms-page__showcases-wrapper showcase-wrapper"></div>
+    </div>
+  );
 };
