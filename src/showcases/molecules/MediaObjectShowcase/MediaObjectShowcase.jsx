b88a34f36345a95640262cddf210870e4ca0717c
marboqmar <martamartinezboquer@gmail.com>
2026-02-02 10:10:59 +0100

    Removed media object background colour and added headings to media object showcase

--- src/showcases/molecules/MediaObjectShowcase/MediaObjectShowcase.jsx
+++ src/showcases/molecules/MediaObjectShowcase/MediaObjectShowcase.jsx
@@ -11,36 +11,42 @@ export const MediaObjectShowcase = () => {
         The following media objects display their media on different positions, according to the
         value of the property <code>mediaLocation</code>.<br />
         On mobile, the media is always displayed on top of the text unless "down" was stated as the{' '}
-        <code>mediaLocation</code>.
+        <code>mediaLocation</code>.<br />
+        Borders are applied only in this showcase, to highlight the component's boundaries.
       </Paragraph>
+      <Heading level={3}>Image up</Heading>
       <MediaObject
         component="section"
         mediaLocation="up"
-        title="Image up"
+        title="Title"
         body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce condimentum fringilla libero, eget convallis tellus consectetur ac"
         imageSrc="/assets/images/test-image-md.png"
       />
+      <Heading level={3}>Image right</Heading>
       <MediaObject
         component="section"
         mediaLocation="right"
-        title="Image right"
+        title="Title"
         body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce condimentum fringilla libero, eget convallis tellus consectetur ac"
         imageSrc="/assets/images/test-image-md.png"
       />
+      <Heading level={3}>Image down</Heading>
       <MediaObject
         component="section"
         mediaLocation="down"
-        title="Image down"
+        title="Title"
         body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce condimentum fringilla libero, eget convallis tellus consectetur ac"
         imageSrc="/assets/images/test-image-md.png"
       />
+      <Heading level={3}>Image left</Heading>
       <MediaObject
         component="section"
         mediaLocation="left"
-        title="Image left"
+        title="Title"
         body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce condimentum fringilla libero, eget convallis tellus consectetur ac"
         imageSrc="/assets/images/test-image-md.png"
       />
+      <Heading level={3}>Video</Heading>
       <MediaObject
         component="section"
         mediaLocation="left"
