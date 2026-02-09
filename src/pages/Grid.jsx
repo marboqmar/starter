0cc0d65f10ce99bfa4c45b94a2e0332073cdc0dc
marboqmar <martamartinezboquer@gmail.com>
2026-02-09 12:21:52 +0100

    Modified grid page structure

--- src/pages/Grid.jsx
+++ src/pages/Grid.jsx
@@ -1,5 +1,13 @@
 import { GridShowcase } from '../showcases/grid/GridShowcase/GridShowcase';
+import { Heading } from '../components/atoms/Heading/Heading';
 
 export const Grid = () => {
-  return <GridShowcase />;
+  return (
+    <div className="grid-page showcase-page container">
+      <Heading level={1} className="showcase-page__title">
+        Grid
+      </Heading>
+      <GridShowcase />
+    </div>
+  );
 };
--- src/showcases/grid/GridShowcase/GridShowcase.jsx
+++ src/showcases/grid/GridShowcase/GridShowcase.jsx
@@ -4,88 +4,95 @@ import { Paragraph } from '../../../components/atoms/Paragraph/Paragraph';
 
 export const GridShowcase = () => {
   return (
-    <div className="container grid-showcase stack gap-md">
-      {/* Responsive examples */}
-      <Heading>Responsive grid layout examples</Heading>
-      <div className="grid-showcase__column grid">
-        <span className="grid-showcase__element col-span-3 col-span-tablet-6">Half</span>
-        <span className="grid-showcase__element col-span-3 col-span-tablet-6">Half</span>
+    <div className="grid-showcase">
+      <div className="grid-showcase__wrapper showcase-item__wrapper">
+        {/* Responsive examples */}
+        <Heading>Responsive grid layout examples</Heading>
+        <div className="grid-showcase__column grid">
+          <span className="grid-showcase__element col-span-3 col-span-tablet-6">Half</span>
+          <span className="grid-showcase__element col-span-3 col-span-tablet-6">Half</span>
+        </div>
+        <div className="grid-showcase__column grid">
+          <span className="grid-showcase__element col-span-2 col-span-tablet-4">Third</span>
+          <span className="grid-showcase__element col-span-2 col-span-tablet-4">Third</span>
+          <span className="grid-showcase__element col-span-2 col-span-tablet-4">Third</span>
+        </div>
+        <div className="grid-showcase__column grid">
+          <span className="grid-showcase__element col-span-1 col-span-tablet-2">Sixth</span>
+          <span className="grid-showcase__element col-span-1 col-span-tablet-2">Sixth</span>
+          <span className="grid-showcase__element col-span-1 col-span-tablet-2">Sixth</span>
+          <span className="grid-showcase__element col-span-1 col-span-tablet-2">Sixth</span>
+          <span className="grid-showcase__element col-span-1 col-span-tablet-2">Sixth</span>
+          <span className="grid-showcase__element col-span-1 col-span-tablet-2">Sixth</span>
+        </div>
       </div>
-      <div className="grid-showcase__column grid">
-        <span className="grid-showcase__element col-span-2 col-span-tablet-4">Third</span>
-        <span className="grid-showcase__element col-span-2 col-span-tablet-4">Third</span>
-        <span className="grid-showcase__element col-span-2 col-span-tablet-4">Third</span>
+      <div className="grid-showcase__wrapper showcase-item__wrapper">
+        {/* Mobile examples */}
+        <Heading>Mobile grid layout examples</Heading>
+        <Paragraph>On mobile, the grid has 6 columns by default.</Paragraph>
+        <div className="grid-showcase__column grid">
+          <span className="grid-showcase__element col-span-3">3/6</span>
+          <span className="grid-showcase__element col-span-3">3/6</span>
+        </div>
+        <div className="grid-showcase__column grid">
+          <span className="grid-showcase__element col-span-2">2/6</span>
+          <span className="grid-showcase__element col-span-2">2/6</span>
+          <span className="grid-showcase__element col-span-2">2/6</span>
+        </div>
+        <div className="grid-showcase__column grid">
+          <span className="grid-showcase__element col-span-1">1/6</span>
+          <span className="grid-showcase__element col-span-1">1/6</span>
+          <span className="grid-showcase__element col-span-1">1/6</span>
+          <span className="grid-showcase__element col-span-1">1/6</span>
+          <span className="grid-showcase__element col-span-1">1/6</span>
+          <span className="grid-showcase__element col-span-1">1/6</span>
+        </div>
       </div>
-      <div className="grid-showcase__column grid">
-        <span className="grid-showcase__element col-span-1 col-span-tablet-2">Sixth</span>
-        <span className="grid-showcase__element col-span-1 col-span-tablet-2">Sixth</span>
-        <span className="grid-showcase__element col-span-1 col-span-tablet-2">Sixth</span>
-        <span className="grid-showcase__element col-span-1 col-span-tablet-2">Sixth</span>
-        <span className="grid-showcase__element col-span-1 col-span-tablet-2">Sixth</span>
-        <span className="grid-showcase__element col-span-1 col-span-tablet-2">Sixth</span>
-      </div>
-      {/* Mobile examples */}
-      <Heading>Mobile grid layout examples</Heading>
-      <Paragraph>On mobile, the grid has 6 columns by default.</Paragraph>
-      <div className="grid-showcase__column grid">
-        <span className="grid-showcase__element col-span-3">3/6</span>
-        <span className="grid-showcase__element col-span-3">3/6</span>
-      </div>
-      <div className="grid-showcase__column grid">
-        <span className="grid-showcase__element col-span-2">2/6</span>
-        <span className="grid-showcase__element col-span-2">2/6</span>
-        <span className="grid-showcase__element col-span-2">2/6</span>
-      </div>
-      <div className="grid-showcase__column grid">
-        <span className="grid-showcase__element col-span-1">1/6</span>
-        <span className="grid-showcase__element col-span-1">1/6</span>
-        <span className="grid-showcase__element col-span-1">1/6</span>
-        <span className="grid-showcase__element col-span-1">1/6</span>
-        <span className="grid-showcase__element col-span-1">1/6</span>
-        <span className="grid-showcase__element col-span-1">1/6</span>
-      </div>
-      {/* Desktop examples */}
-      <Heading>Desktop grid layout examples</Heading>
-      <Paragraph>
-        On desktop, the grid has 12 columns by default. On mobile, the following examples are set to
-        occupy the whole grid.
-      </Paragraph>
-      <div className="grid-showcase__column grid">
-        <span className="grid-showcase__element col-span-6 col-span-tablet-6">6/12</span>
-        <span className="grid-showcase__element col-span-6 col-span-tablet-6">6/12</span>
-      </div>
-      <div className="grid-showcase__column grid">
-        <span className="grid-showcase__element col-span-6 col-span-tablet-4">4/12</span>
-        <span className="grid-showcase__element col-span-6 col-span-tablet-4">4/12</span>
-        <span className="grid-showcase__element col-span-6 col-span-tablet-4">4/12</span>
-      </div>
-      <div className="grid-showcase__column grid">
-        <span className="grid-showcase__element col-span-6 col-span-tablet-3">3/12</span>
-        <span className="grid-showcase__element col-span-6 col-span-tablet-3">3/12</span>
-        <span className="grid-showcase__element col-span-6 col-span-tablet-3">3/12</span>
-        <span className="grid-showcase__element col-span-6 col-span-tablet-3">3/12</span>
-      </div>
-      <div className="grid-showcase__column grid">
-        <span className="grid-showcase__element col-span-6 col-span-tablet-2">2/12</span>
-        <span className="grid-showcase__element col-span-6 col-span-tablet-2">2/12</span>
-        <span className="grid-showcase__element col-span-6 col-span-tablet-2">2/12</span>
-        <span className="grid-showcase__element col-span-6 col-span-tablet-2">2/12</span>
-        <span className="grid-showcase__element col-span-6 col-span-tablet-2">2/12</span>
-        <span className="grid-showcase__element col-span-6 col-span-tablet-2">2/12</span>
-      </div>
-      <div className="grid-showcase__column grid">
-        <span className="grid-showcase__element col-span-6 col-span-tablet-1">1/12</span>
-        <span className="grid-showcase__element col-span-6 col-span-tablet-1">1/12</span>
-        <span className="grid-showcase__element col-span-6 col-span-tablet-1">1/12</span>
-        <span className="grid-showcase__element col-span-6 col-span-tablet-1">1/12</span>
-        <span className="grid-showcase__element col-span-6 col-span-tablet-1">1/12</span>
-        <span className="grid-showcase__element col-span-6 col-span-tablet-1">1/12</span>
-        <span className="grid-showcase__element col-span-6 col-span-tablet-1">1/12</span>
-        <span className="grid-showcase__element col-span-6 col-span-tablet-1">1/12</span>
-        <span className="grid-showcase__element col-span-6 col-span-tablet-1">1/12</span>
-        <span className="grid-showcase__element col-span-6 col-span-tablet-1">1/12</span>
-        <span className="grid-showcase__element col-span-6 col-span-tablet-1">1/12</span>
-        <span className="grid-showcase__element col-span-6 col-span-tablet-1">1/12</span>
+
+      <div className="grid-showcase__wrapper showcase-item__wrapper">
+        {/* Desktop examples */}
+        <Heading>Desktop grid layout examples</Heading>
+        <Paragraph>
+          On desktop, the grid has 12 columns by default. On mobile, the following examples are set
+          to occupy the whole grid.
+        </Paragraph>
+        <div className="grid-showcase__column grid">
+          <span className="grid-showcase__element col-span-6 col-span-tablet-6">6/12</span>
+          <span className="grid-showcase__element col-span-6 col-span-tablet-6">6/12</span>
+        </div>
+        <div className="grid-showcase__column grid">
+          <span className="grid-showcase__element col-span-6 col-span-tablet-4">4/12</span>
+          <span className="grid-showcase__element col-span-6 col-span-tablet-4">4/12</span>
+          <span className="grid-showcase__element col-span-6 col-span-tablet-4">4/12</span>
+        </div>
+        <div className="grid-showcase__column grid">
+          <span className="grid-showcase__element col-span-6 col-span-tablet-3">3/12</span>
+          <span className="grid-showcase__element col-span-6 col-span-tablet-3">3/12</span>
+          <span className="grid-showcase__element col-span-6 col-span-tablet-3">3/12</span>
+          <span className="grid-showcase__element col-span-6 col-span-tablet-3">3/12</span>
+        </div>
+        <div className="grid-showcase__column grid">
+          <span className="grid-showcase__element col-span-6 col-span-tablet-2">2/12</span>
+          <span className="grid-showcase__element col-span-6 col-span-tablet-2">2/12</span>
+          <span className="grid-showcase__element col-span-6 col-span-tablet-2">2/12</span>
+          <span className="grid-showcase__element col-span-6 col-span-tablet-2">2/12</span>
+          <span className="grid-showcase__element col-span-6 col-span-tablet-2">2/12</span>
+          <span className="grid-showcase__element col-span-6 col-span-tablet-2">2/12</span>
+        </div>
+        <div className="grid-showcase__column grid">
+          <span className="grid-showcase__element col-span-6 col-span-tablet-1">1/12</span>
+          <span className="grid-showcase__element col-span-6 col-span-tablet-1">1/12</span>
+          <span className="grid-showcase__element col-span-6 col-span-tablet-1">1/12</span>
+          <span className="grid-showcase__element col-span-6 col-span-tablet-1">1/12</span>
+          <span className="grid-showcase__element col-span-6 col-span-tablet-1">1/12</span>
+          <span className="grid-showcase__element col-span-6 col-span-tablet-1">1/12</span>
+          <span className="grid-showcase__element col-span-6 col-span-tablet-1">1/12</span>
+          <span className="grid-showcase__element col-span-6 col-span-tablet-1">1/12</span>
+          <span className="grid-showcase__element col-span-6 col-span-tablet-1">1/12</span>
+          <span className="grid-showcase__element col-span-6 col-span-tablet-1">1/12</span>
+          <span className="grid-showcase__element col-span-6 col-span-tablet-1">1/12</span>
+          <span className="grid-showcase__element col-span-6 col-span-tablet-1">1/12</span>
+        </div>
       </div>
     </div>
   );
