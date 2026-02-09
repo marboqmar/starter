e32f556afd65339e3532ca1363249a78b18d46ca
marboqmar <martamartinezboquer@gmail.com>
2026-02-09 12:23:55 +0100

    Added breadcrumbs to grid page

--- src/pages/Grid.jsx
+++ src/pages/Grid.jsx
@@ -1,9 +1,17 @@
 import { GridShowcase } from '../showcases/grid/GridShowcase/GridShowcase';
 import { Heading } from '../components/atoms/Heading/Heading';
+import { Breadcrumbs } from '../components/molecules/Breadcrumbs/Breadcrumbs';
+
+const breadcrumbsInfo = [
+  { label: 'Home', href: '/' },
+  { label: 'Grid', href: '/grid' },
+];
 
 export const Grid = () => {
   return (
     <div className="grid-page showcase-page container">
+      <Breadcrumbs items={breadcrumbsInfo} />
+
       <Heading level={1} className="showcase-page__title">
         Grid
       </Heading>
